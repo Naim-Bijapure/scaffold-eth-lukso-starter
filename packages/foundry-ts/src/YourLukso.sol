7c7e6f0908ee16c8f575fc4083466cf7f135d01c
NaimBijapure <naim@vinnovatetechnologies.com>
2022-07-21 23:15:33 +0530

    Title: update: working code from docs

--- packages/foundry-ts/src/YourLukso.sol
+++ packages/foundry-ts/src/YourLukso.sol
@@ -1,10 +1,15 @@
-import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+pragma solidity >=0.8.0 <0.9.0;
 
-contract YourLukso is LSP0ERC725Account {
+//SPDX-License-Identifier: MIT
+
+// import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+
+// contract YourLukso is LSP0ERC725Account {
+contract YourLukso {
   // constructor(address _newOwner) LSP0ERC725Account(_newOwner) {}
-  constructor(address _newOwner) LSP0ERC725Account(_newOwner) {}
+  constructor() {}
 
-  string purpose = "default purpose";
+  string public purpose = "default purpose";
 
   function setPurpose(string memory _purpose) public {
     purpose = _purpose;
--- packages/next-ts/pages/Poc.tsx
+++ packages/next-ts/pages/Poc.tsx
@@ -13,17 +13,18 @@ import useAppLoadContract from "../hooks/useAppLoadContract";
 import { useStore } from "../store/useStore";
 
 // Our static variables
-const SAMPLE_PROFILE_ADDRESS = "0x27aA856743f2e5FCd277C75e605342e0e097e5C8";
-// const RPC_ENDPOINT = "https://rpc.l16.lukso.network";
-const RPC_ENDPOINT = "http://0.0.0.0:8545";
+const SAMPLE_PROFILE_ADDRESS = "0xB990CfE5474F42fee8c22ee0852f1110a3E798e1";
+const RPC_ENDPOINT = "https://rpc.l16.lukso.network";
+// const RPC_ENDPOINT = "http://0.0.0.0:8545";
 const IPFS_GATEWAY = "https://2eff.lukso.dev/ipfs/";
+const yourLuksoAddress = "0xf4c29832c29723b70Fb77729dA9187250C9d5A90";
 
 const yourLuksotBytecode = YourLukso__factory.bytecode;
 
 export default function PocPage(): ReactElement {
   // const { ethPrice, usdPrice } = useDexPrice();
   // console.log("ethPrice: ", ethPrice, usdPrice);
-  const { data, isSuccess } = useAccount();
+  const { data: mainAccount, isSuccess } = useAccount();
   const provider = useProvider();
   const { data: signer } = useSigner();
   const [state, dispatch] = useStore();
@@ -34,27 +35,30 @@ export default function PocPage(): ReactElement {
 
   const onFactoryDeploy = async (): any => {
     try {
+      // const MAIN_PRIVATE_KEY = "d8bc935f06c027c7e0edeedcc46593554dbb3e9e01101f4f3b8ab69e36e2e484";
       /** ----------------------
        *
        *TO CREATE UP
        * ---------------------*/
       const lspFactory = new LSPFactory(RPC_ENDPOINT, {
         deployKey: account.privateKey,
+        // deployKey: MAIN_PRIVATE_KEY,
         chainId: 2828,
       });
+
       // @ts-ignore
       async function createUniversalProfile(): any {
         console.log("createUniversalProfile:creating ");
         const deployedContracts = await lspFactory.UniversalProfile.deploy({
-          controllerAddresses: [account.address], // our EOA that will be controlling the UP
+          controllerAddresses: [account.address, mainAccount?.address as string], // our EOA that will be controlling the UP
           lsp3Profile: {
-            name: "My Universal Profile",
-            description: "My Cool Universal Profile",
-            tags: ["Public Profile"],
+            name: "naim's profile",
+            description: "this is naims cool profile ",
+            tags: ["Public Profile cool test"],
             links: [
               {
                 title: "My Website",
-                url: "https://my-website.com",
+                url: "https://naimbijapure.eth",
               },
             ],
           },
@@ -83,7 +87,11 @@ export default function PocPage(): ReactElement {
       /** ----------------------
        * FETCH PROFILE DATA
        * ---------------------*/
-      const provider = window.ethereum;
+      //@ts-ignore
+      const etherProvider = new ethers.providers.Web3Provider(window.ethereum);
+
+      // const provider = window.ethereum;
+      const provider = etherProvider;
       const config = { ipfsGateway: IPFS_GATEWAY };
       // @ts-ignore
       async function fetchProfile(address): any {
@@ -106,34 +114,88 @@ export default function PocPage(): ReactElement {
 
   const onDeployOtherContract = async (): any => {
     try {
-      // const yourLukso = new YourLukso__factory(signer as Signer);
-      // const yDeploy = await yourLukso.deploy(account.address);
-      const targetPayload = yourLukso?.interface.encodeFunctionData("setPurpose", ["cool purpose"]);
-      console.log("setPurposeEncoded: ", targetPayload);
       const OPERATION_CALL = 0;
 
-      const wallet = new ethers.Wallet(account);
-      console.log("wallet: ", wallet.address);
+      const myProvider = provider;
+      const mySigner = signer as Signer;
 
-      const pSigner = signer?.connect(wallet.provider);
+      // const myProvider = new ethers.Wallet(account.privateKey, provider);
+      // signer?.connect(myProvider.provider);
+      // const mySigner = myProvider.getSigner(account.address);
+      // console.log("mySigner: ", mySigner);
+      // console.log("signer: ", await mySigner?.getAddress());
 
-      const myUP = new ethers.Contract(SAMPLE_PROFILE_ADDRESS, UniversalProfile.abi, pSigner as Signer);
+      // contracts
+      const myUP = new ethers.Contract(SAMPLE_PROFILE_ADDRESS, UniversalProfile.abi, myProvider);
       console.log("myUP: ", myUP);
 
-      const myKM = new ethers.Contract(account.address, KeyManager.abi, pSigner as Signer);
-      console.log("myKM: ", myKM);
+      const ownerKM = await myUP?.owner();
+      console.log("ownerKM: ", ownerKM);
+
+      const myKM = new ethers.Contract(ownerKM as string, KeyManager.abi, myProvider);
+      console.log("myKM: ", myKM.address);
+
+      const targetContract = new ethers.Contract(yourLuksoAddress, YourLukso__factory.abi, myProvider);
+      console.log("yourLukso: ", targetContract.address);
+
+      /** ----------------------
+       * transfer token code
+       * ---------------------*/
+      // const recipient = account.address; // address the recipient (any address, including an other UP)
+      // const amount = ethers.utils.parseUnits("1", "ether");
+      // console.log("amount: ", amount);
+      // // payload executed at the target (here nothing, just a plain LYX transfer)
+      // const data = "0x";
+
+      // const transferPayload = myUP?.interface.encodeFunctionData("execute", [OPERATION_CALL, recipient, amount, data]);
+      // console.log("transferPayload: ", transferPayload);
+
+      // const response = await myKM.connect(signer as Signer).execute(transferPayload, { gasLimit: 10000000 });
+
+      /** ----------------------
+       * CALL OTHER FROM UP COD
+       * ---------------------*/
+      const targetPayload = targetContract.interface.encodeFunctionData("setPurpose", ["cool man from up"]);
+      console.log("targetPayload: ", targetPayload);
 
       const abiPayload = myUP.interface.encodeFunctionData("execute", [
         OPERATION_CALL,
-        yourLukso?.address,
+        targetContract.address,
         0,
         targetPayload,
       ]);
 
-      const response = await myKM.connect(account.address).execute(abiPayload);
-      console.log("response: ", response);
-      // console.log("response: ", response);
-      // console.log("abiPayload: ", abiPayload);
+      console.log("abiPayload: ", abiPayload);
+      // const tx = await myKM.connect(signer as Signer).execute(abiPayload, { gasLimit: 10000000 });
+      // const rcpt = await tx.wait();
+      // console.log("rcpt: ", rcpt);
+
+      const data = await targetContract.purpose();
+      console.log("purpose data: ", data);
+
+      /** ----------------------
+       * DIRECT CONTRACT CALL
+       * ---------------------*/
+      // const targetContract = new ethers.Contract(yourLuksoAddress, YourLukso__factory.abi, myProvider);
+      // console.log("yourLukso: ", targetContract.address);
+
+      // const data = await targetContract.purpose();
+      // console.log("data: ", data);
+
+      // const tx = await targetContract.connect(signer as Signer).setPurpose("updated man");
+      // const rcpt = await tx.wait();
+      // console.log("rcpt: ", rcpt);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  const onDeployYourLukso = async (): any => {
+    try {
+      const yourLoksoFactory = new YourLukso__factory(signer as Signer);
+      const yourLukso = await yourLoksoFactory.deploy();
+      console.log("yourLukso: ", yourLukso.address);
+      const data = await yourLukso.pu;
     } catch (error) {
       console.log("error: ", error);
     }
@@ -150,8 +212,12 @@ export default function PocPage(): ReactElement {
           on fetch profile
         </button>
 
+        <button onClick={onDeployYourLukso} className="m-2 btn btn-primary">
+          deploy yourLokso Contract
+        </button>
+
         <button onClick={onDeployOtherContract} className="m-2 btn btn-primary">
-          deploy other contract
+          interact other contract
         </button>
       </div>
     </>
